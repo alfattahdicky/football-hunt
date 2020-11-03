dd10e5437c84a9b40f6ad95da922d5bd0753ce31
dicky al fattah <alfattahdicky2@gmail.com>
2020-11-03 20:54:37 +0700

    first commit

---
+++ script/nav.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const navbar = document.querySelectorAll('.sidenav');
+	M.Sidenav.init(navbar);
+	loadNav();
+
+	function loadNav() {
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4) {
+				if (this.status !== 200) return;
+				document.querySelectorAll('.topnav, .sidenav').forEach((e) => {
+					e.innerHTML = xhttp.responseText;
+				});
+				document.querySelectorAll('.sidenav a, .topnav a').forEach((e) => {
+					e.addEventListener('click', function (event) {
+						const sidenav = document.querySelector('.sidenav');
+						M.Sidenav.getInstance(sidenav).close();
+
+						page = event.target.getAttribute('href').substr(1);
+						loadPage(page);
+					});
+				});
+			}
+		};
+		xhttp.open('GET', 'nav.html', true);
+		xhttp.send();
+	}
+	let page = window.location.hash.substr(1);
+	if (page === '') page = 'home';
+	loadPage(page);
+
+	function loadPage(page) {
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState === 4) {
+        let content = document.querySelector('.body-content');
+				if (this.status === 200) {
+          content.innerHTML = xhttp.responseText;
+          // getAllStandings();
+          // Slider
+          if(page === 'home') {
+            const slider = document.querySelectorAll('.slider');
+            M.Slider.init(slider, {
+              indicators: false,
+              height: 450
+            })
+          }
+				} else if (this.status === 404) {
+					content.innerHTML = '<p>Page not found.</p>';
+				} else {
+					content.innerHTML = '<p>Ups.. page cannot be access';
+				}
+			}
+		};
+		xhttp.open('GET', `pages/${page}.html`, true);
+		xhttp.send();
+	}
+});
+
+  
+  
+  
