8d8d494ca166b58dec4812841535420645203be1
dicky al fattah <alfattahdicky2@gmail.com>
2020-11-16 03:00:07 +0700

    use plugin workbox

--- service-worker.js
+++ service-worker.js
@@ -1,95 +1,144 @@
-const CACHE_NAME = 'football-hunt-v3';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/nav.html',
-  '/team.html',
-  '/push.js',
-  '/service-worker.js',
-  '/manifest.json',
-  '/pages/home.html',
-  '/pages/match.html',
-  '/pages/saved.html',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-	'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
-  '/style/materialize.min.css',
-  '/style/style.css',
-  '/script/api.js',
-  '/script/db.js',
-  '/script/idb.js',
-  '/script/materialize.min.js',
-  '/script/nav.js',
-  '/script/script.js',
-  '/script/sw-register.js',
-  '/assets/1.jpg',
-  '/assets/2.jpg',
-  '/assets/3.jpg',
-  '/assets/icon/icon-144x144.png',
-  '/assets/icon/icon-192x192.svg',
-  '/assets/icon/icon-512x512.png',
-]
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+if (workbox) console.log('Workbox Berhasil dimuat');
+else console.log('Workbox gagal dimuat');
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+workbox.precaching.precacheAndRoute([{
+    url: '/index.html',
+    revision: '1'
+  },
+  {
+    url: '/nav.html',
+    revision: 'null'
+  },
+  {
+    url: '/team.html',
+    revision: 'null'
+  },
+  {
+    url: '/push.js',
+    revision: 'null'
+  },
+  {
+    url: '/manifest.json',
+    revision: 'null'
+  },
+  {
+    url: '/style/materialize.min.css',
+    revision: 'null'
+  },
+  {
+    url: '/style/style.css',
+    revision: 'null'
+  },
+  {
+    url: '/script/api.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/db.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/idb.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/materialize.min.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/nav.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/script.js',
+    revision: 'null'
+  },
+  {
+    url: '/script/sw-register.js',
+    revision: 'null'
+  },
+], {
+  ignoreUrlParameterMatching: [/.*/],
+})
+
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'pages',
+  })
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'assets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://api.football-data.org'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'api-football',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 30
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.gstatic.com/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-font',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+        maxEntries: 30
+      })
+    ]
+  })
+)
 
 
-//Menyimpan cache secara dinamis
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request, {cacheName:CACHE_NAME, ignoreSearch : true})
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      const fetchRequest = event.request.clone();
-      return fetch(fetchRequest).then(
-        function(response) {
-          if(!response || response.status !== 200) {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        }
-      );
-    })
-  );
-});
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME && cacheName.startsWith("football-hunt")) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+
+// const CACHE_NAME = 'football-hunt-v3';
+// const urlsToCache = [
+//   '/manifest.json',
+//   '/pages/home.html',
+//   '/pages/match.html',
+//   '/pages/saved.html',
+//   'https://fonts.googleapis.com/icon?family=Material+Icons',
+// 	'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+//   '/style/style.css',
+//   '/assets/1.jpg',
+//   '/assets/2.jpg',
+//   '/assets/3.jpg',
+//   '/assets/icon/icon-144x144.png',
+//   '/assets/icon/icon-192x192.svg',
+//   '/assets/icon/icon-512x512.png',
+// ]
 
 self.addEventListener('push', event => {
   let body;
-  if(event.data) {
+  if (event.data) {
     body = event.data.text();
   } else {
-    body ="Push message no payload";
+    body = "Push message no payload";
   }
   const title = 'Pertandingan Berakhir';
   const options = {
     body: body,
     icon: 'assets/icon/icon-144x144.png',
-    vibrate: [100,50,100],
+    vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
       primaryKey: 1
